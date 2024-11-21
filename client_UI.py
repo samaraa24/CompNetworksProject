0e3a5562de1c321806f4a10e073f7b200a14f3d6
aschafer181 <adamdschafer@gmail.com>
2024-11-20 20:45:19 -0500

    Update client_UI.py
    added change directory functionality and file name input for download

--- client_UI.py
+++ client_UI.py
@@ -15,51 +15,61 @@ PORT = 4450
 SIZE = 1024
 FORMAT = "utf-8"
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+current_directory = ""
 
 # create windows
 root = Tk()
-file_window = Toplevel(root)
 
 # main window title and dimension 
 root.title("File Sharing Cloud Service")
 root.geometry('600x500')
 
-# stating the minimum file sizes in bytes 
-MIN_FILE_SIZES = {
+# stating the maximum file sizes in bytes 
+MAX_FILE_SIZES = {
     ".txt" : 25 * 1024 * 1024, # 25 MB
     ".mp3" : 0.5 * 1024 * 1024 * 1024, #0.5 GB
     ".mp4" : 2 * 1024 * 1024 * 1024 #2 GB
 }
 
+
 # commmand when user wants to upload a file 
 def upload_file():
-    #allows user to upload text, audio, and video files 
+    # allows user to upload text, audio, and video files 
     filename = filedialog.askopenfilename(title= "Select a file", filetypes=[("Text files", ".txt"),
                                                                              ("Audio files", ".mp3"), 
                                                                              ("Video files", ".mp4")])
+
+    # returns if no file is selected
+    if not filename:
+        return
  
-    #checks for file extension chosen to connect it to it's file size
+    # checks for file extension chosen to connect it to it's file size
     file_extension = os.path.splitext(filename)[1]
     file_size = os.path.getsize(filename)       
 
-    #sending error message if user selects incorrect file size 
-    if file_size < MIN_FILE_SIZES[file_extension]:
+    # sending error message if user selects incorrect file size 
+    while file_size > MAX_FILE_SIZES[file_extension]:
         messagebox.showerror("Error", "File does not meet size requirement")
         filename = filedialog.askopenfilename(title= "Select a file", filetypes=[("Text files", ".txt"),
                                                                              ("Audio files", ".mp3"), 
                                                                              ("Video files", ".mp4")])
+        # returns if no file is selected
+        if not filename:
+            return
 
     f = open(filename, 'rb')
     print('Selected:', filename)
 
+    file_path = os.path.join(current_directory, os.path.basename(filename))
+
     # sends upload command with file name and size
-    s.send(f"UPLOAD@{os.path.basename(filename)}@{os.path.getsize(filename)}".encode(FORMAT))
+    s.send(f"UPLOAD@{file_path}@{file_size}".encode(FORMAT))
 
     # sends data to server
-    sendData = f.read(1024)
+    sendData = f.read(SIZE)
     while (sendData):
         s.send(sendData)
-        sendData = f.read(1024)
+        sendData = f.read(SIZE)
     f.close()
 
     # receives status from server
@@ -67,10 +77,12 @@ def upload_file():
     status, msg = data.split("@")
     print(status,": ",msg)
 
+
 # command when user wants to download a file
 def download_file():
-    filename = "text2.txt" # placeholder for filename input
-    s.send(f"DOWNLOAD@{filename}".encode(FORMAT))
+    filename = simpledialog.askstring("Download File","Enter File Name")
+    file_path = os.path.join(current_directory, filename)
+    s.send(f"DOWNLOAD@{file_path}".encode(FORMAT))
     data = s.recv(SIZE).decode(FORMAT)
     status, msg = data.split("@")
 
@@ -87,35 +99,36 @@ def download_file():
         print(f"Download of {filename} complete.")
 
     else:
-        print(status,": ", msg)
+        print(status,": ",msg)
 
 
 def view_all_files():
-
-    #sending command to server 
+    file_window = Toplevel(root)
+    
+    # sending command to server 
     s.send("DIR@".encode(FORMAT))
 
-    #receiving response 
+    # receiving response 
     response = s.recv(SIZE).decode(FORMAT)
     status, files = response.split("@",1)
 
-    #new window will open with the file database
+    # new window will open with the file database
     file_window.title("File Database")
     file_window.geometry('600x500')
 
-    #making listbox inside new window to display the files 
+    # making listbox inside new window to display the files 
     file_listbox = Listbox(file_window,height=15, 
                            width=50, bg="lightgrey", 
                            font=("Helvetica", 12), fg="black")
     file_listbox.pack(pady = 10, padx = 10, fill = BOTH, expand = True)
 
-    #adding a scrollbar 
+    # adding a scrollbar 
     scrollbar = Scrollbar(file_window)
     scrollbar.pack(side = RIGHT, fill = BOTH)
     file_listbox.config(yscrollcommand=scrollbar.set)
     scrollbar.config(command=file_listbox.yview)
 
-    #when it finds the files, if the server isnt empty, the files will be displayed 
+    # when it finds the files, if the server isnt empty, the files will be displayed 
     if status == "OK":
 
         if files.strip():
@@ -131,21 +144,20 @@ def view_all_files():
         file_listbox.insert(END,f"Error: {files}")
 
 
-
 def delete_files():
-    file_window
+    file_window = Toplevel(root)
 
     filename = simpledialog.askstring("Delete Files", "Enter File Name")
    
     if filename:
-        #sending delete command to server
+        # sending delete command to server
         s.send(f"DELETE@{filename}".encode(FORMAT))
 
-        #receive server's response 
-        response = s.resv(SIZE).decode(FORMAT)
+        # receive server's response 
+        response = s.recv(SIZE).decode(FORMAT)
         status, message = response.split("@", 1)
 
-        #server response 
+        # server response 
         if status == "OK":
             messagebox.showinfo(f"{filename} was successfully deleted", message)
         else:
@@ -155,7 +167,7 @@ def delete_files():
 
 
 def create_directory():
-    file_window
+    file_window = Toplevel(root)
 
     filename = simpledialog.askstring("Create Directories", "Enter Directory Name")
 
@@ -164,7 +176,7 @@ def create_directory():
         s.send(f"CREATEDIR@{filename}".encode(FORMAT))
 
         # receive server's response
-        response = s.resv(SIZE).decode(FORMAT)
+        response = s.recv(SIZE).decode(FORMAT)
         status, message = response.split("@", 1)
 
         #server response 
@@ -175,6 +187,32 @@ def create_directory():
     else: 
         print("Directory already exists")
 
+
+def change_dir():
+    global current_directory
+    s.send("DIR@".encode(FORMAT))
+    
+    # receiving response 
+    response = s.recv(SIZE).decode(FORMAT)
+    status, folders = response.split("@",1)
+
+    if status == "OK":
+        folder_name = simpledialog.askstring("Change Directory","Enter Directory Name ('..' For Parent Directory)")
+        if folder_name and folder_name in folders:
+            current_directory = os.path.join(current_directory, folder_name)
+            print(current_directory)
+
+        elif folder_name == ".." and current_directory:
+            current_directory = os.path.dirname(current_directory)
+            print(current_directory)
+            
+        else:
+            messagebox.showerror("Error","Error: Directory does not exist.")
+
+    else:
+        messagebox.showerror(status,": ",folders)
+            
+
 # from main window to upload window where user would actually upload files after connecting to server 
 def connect_server():
     # connecting to server 
@@ -183,7 +221,7 @@ def connect_server():
     status, msg = data.split("@")
     
 
-    connect.configure(text = msg) #stand in text for the actual action
+    connect.configure(text = msg) # stand in text for the actual action
     connect.place(x = 180, y = 0)
 
     # menu of options
@@ -209,6 +247,7 @@ def connect_server():
     view_files.pack()
     view_files.place(x = 235, y = 90)
 
+    # delete files button
     delete = Button(root, text = "Delete Files", fg = "blue", bg = "white", command = delete_files)
     delete.pack()
     delete.place(x = 240, y = 120)
@@ -217,12 +256,18 @@ def connect_server():
     createDirectory = Button(root, text = "Create New Directory", fg = "blue", bg = "white", command = create_directory)
     createDirectory.pack()
     createDirectory.place(x = 210, y = 150)
-        
+
+    # change directory button
+    delete = Button(root, text = "Change Directory", fg = "blue", bg = "white", command = change_dir)
+    delete.pack()
+    delete.place(x = 220, y = 180)
+    
     listbox.insert(1, upload)
     listbox.insert(2, download)
     listbox.insert(3, view_files)
     listbox.insert(4, delete)
     listbox.insert(5, createDirectory)
+    listbox.insert(6, change_dir)
 
 
 # connect server IP port -> initiate connection from client to server with specified files 
